604a083f34798fb22bc18dbd9b678fa203c3b1c3
Ianwp <ian@browserstack.com>
2021-09-30 09:46:34 -0700

    Update geolocation.js

--- specs/geolocation.js
+++ specs/geolocation.js
@@ -1,13 +1,13 @@
 describe('GeoLocation', () => {
-  it('should load German version of PayPal button', () => {
-      browser.url('https://paypal.github.io/react-paypal-js/iframe.html?id=example-venmobutton--horizontal&viewMode=story');
+  it('should load German version of button', () => {
+      browser.url('EXAMPLE');
 
       const frame = $('/html/body/div[3]/div/div/iframe[1]');
       frame.waitForDisplayed();
 
       browser.switchToFrame(frame);
 
-      const message = $('.paypal-button-tagline');
+      const message = $('.button-tagline');
  
       expect(message).toHaveText('Überall schnell und sicher bezahlen');
   });
